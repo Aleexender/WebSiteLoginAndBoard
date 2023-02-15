7fa746da35c01de0d5dffa95bad4df2009dee41d
Alex Kim <alexkjh2011@gmail.com>
2023-02-15 09:29:00 +0900

    Change class

--- src/main/java/com/example/websiteproject1/HomeController.java
+++ src/main/java/com/example/websiteproject1/HomeController.java
@@ -6,28 +6,9 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
-
-    @GetMapping(path = "/login")
-    public String home() {
-        return "login";
+    @GetMapping("/")
+    public void home() {
     }
-
-    @GetMapping(path = "/signUp")
-    public String home2() {
-        return "signUp";
-    }
-
-
-    @GetMapping(path = "/signUp2")
-    public ModelAndView home3(String id ) {
-        ModelAndView modelAndView = new ModelAndView();
-
-        modelAndView.setViewName("signUp");
-        modelAndView.addObject("id", id);
-
-        return modelAndView;
-    }
-
 }
 
 
